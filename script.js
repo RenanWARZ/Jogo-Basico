131c13d37c80e5b6e2073addacf9efbe5c471349
RenanWARZ <renanpilan711@gmail.com>
2025-04-03 08:38:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const mario = document.querySelector('.mario');  /*selecionando a class mario */
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump'); /* aqui ele está adicionando a class jump, para pode fazer a animação */
+
+    setTimeout(() => {
+        mario.classList.remove('jump'); /* aqui estamos removendo a animação assim que acabar de animar, ou seja, a hora que acabar os 500ms, ele remove e começa denovo */
+    }, 800);
+}
+
+const loop = setInterval(() =>{
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const gameOverBtn = document.getElementById('gameOverBtn');
+    
+    if(pipePosition <= 150 && pipePosition > 0 && marioPosition < 100 ){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src='lose.gif';
+        mario.style.width ='155px'
+        mario.style.marginLeft = '0px'
+
+
+        clearInterval(loop);
+
+        gameOverBtn.style.display = 'block';
+
+        
+    }
+
+}, 10)
+    document.addEventListener('keydown',jump); /* aqui colocamos para apertar uma tecla para pular */
+
+    function restartGame() {
+        location.reload();
+    }
